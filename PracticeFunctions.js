7250882141f2f056a200b3eb870575f4d58b9db5
Christa Stephens <ChristaStephens@outlook.com>
2019-02-04 07:45:02 -0500

    more functions

--- PracticeFunctions.js
+++ PracticeFunctions.js
@@ -117,6 +117,21 @@ var todoList =
   	//when grabbing the todo, put in the correct postion of index.
 
   	this.displayTodos();
+  },
+  toggleAll: function(){
+  	var totalTodos = this.todos.length;
+  	var completedTodos = 0;
+
+  	for( var i=0; i<totalTodos; i++){
+  		if(this.todos[i].completed===true){
+  			completedTodos++;
+  		}
+  	}
+  	if (completedTodos===totalTodos){
+  		for( var i=0; i<totalTodos; i++){
+  			this.todos[i].completed=false;
+  		}
+  	}
   }
 };
 
